f1d8dec69934a878c41d5fd061cf650b1df4018f
Mohamedawad-code <awadmohamed7000@gmail.com>
2025-08-07 12:17:06 +0200

    Add Game Foundation Ive added the option to choose Rock, paper Scissors. Also ability to determine a winner, but no point system yet or round

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// the computers choice
 function getComputerChoice() {
     const choices ={
         0: "Rock",
@@ -8,12 +10,52 @@ function getComputerChoice() {
 
 
 }
-
+// The human/players choice
 function getHumanChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     return prompt("Enter your choice (Rock, Paper, Scissors):");
 }
 
-
+// the results in console log
 console.log(getComputerChoice());
 console.log(getHumanChoice());
+
+//the score
+let Humanscore = 0;
+let Computerscore = 0;
+
+
+// *here we setup point system, and hiarchy of the game
+function determineWinner(getHumanChoice, getComputerChoice) {
+    if (getHumanChoice === getComputerChoice) {
+        return "tie";
+    } else if (getHumanChoice === "Rock" && getComputerChoice === "Scissors"){
+        return "Player Wins";
+    }
+      else if (getHumanChoice === "Paper" && getComputerChoice === "Rock") {
+        return "Player Wins";
+    } else if (getHumanChoice === "Scissors" && getComputerChoice === "Paper") {
+        return "Player Wins";
+    } else {
+        return "Computer Wins";
+    }
+
+
+}
+
+console.log(determineWinner(getHumanChoice, getComputerChoice));
+
+const winner = determineWinner(getHumanChoice(), getComputerChoice());
+
+function updateScore(winner) {
+    if (winner === "Player Wins") {
+        Humanscore++;
+    } else if (winner === "Computer Wins") {
+        Computerscore++;
+    }
+}
+
+console.log("Human Score:" + Humanscore);
+console.log("Computer Score:" + Computerscore);
+
+//*here will setup the game loop and determine the winner */
